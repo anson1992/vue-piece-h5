18da4ed17ee8205893866fda94b48074e9c391f3
roman <changjun19920716@gmail.com>
2021-10-19 17:53:25 +0800

    ✨ 基于vue-skelotor实现的骨架屏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home-index">
-    <cell-group title="基础功能">
-      <cell title="接口请求" is-link to="/api-group"></cell>
-    </cell-group>
+    <div v-if="isLoading">
+      <home-skeletor></home-skeletor>
+    </div>
+    <div v-else>
+      <cell-group title="基础功能">
+        <cell title="接口请求" is-link to="/api-group"></cell>
+      </cell-group>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-  import { onMounted } from 'vue'
+  import { onMounted, ref, Ref } from 'vue'
   import { CellGroup, Cell } from 'vant'
+  import HomeSkeletor from './skeleton.vue'
+  const isLoading: Ref<boolean> = ref(true)
   onMounted(() => {
     document.dispatchEvent(new Event('render-event'))
+    // setTimeout(() => {
+    //   isLoading.value = false
+    // }, 4000)
   })
 </script>
 <style lang="scss" scoped>
--- src/views/home/skeleton.vue
+++ src/views/home/skeleton.vue
@@ -1,16 +1,75 @@
 <template>
-  <div class="home-skeleton-index"></div>
+  <div class="home-skeleton-index">
+    <skeletor height="200"></skeletor>
+    <div class="card__slim">
+      <div class="card__avatar">
+        <Skeletor circle size="48" class="post__avatar" />
+      </div>
+
+      <div class="card__meta">
+        <h2>
+          <Skeletor width="50%" />
+        </h2>
+
+        <p>
+          <Skeletor width="75%" />
+        </p>
+      </div>
+    </div>
+    <div class="card__text">
+      <Skeletor v-for="i in 5" :key="i" />
+    </div>
+
+    <div class="card__actions">
+      <Skeletor width="96" height="36" pill style="margin-right: 4px" />
+      <Skeletor width="96" height="36" pill />
+    </div>
+  </div>
 </template>
 <script lang="ts">
   import { defineComponent } from 'vue'
+  import 'vue-skeletor/dist/vue-skeletor.css'
+  import { Skeletor } from 'vue-skeletor'
   export default defineComponent({
-    name: 'ComponentName',
+    name: 'HomeSkeletor',
+    components: {
+      Skeletor
+    },
     setup() {
       return {}
     }
   })
 </script>
 <style lang="scss" scoped>
-  .page {
+  .home-skeleton-index {
+    .card__slim {
+      display: flex;
+      margin-top: 16px;
+      padding: 0 16px;
+      box-sizing: border-box;
+      .card__avatar {
+        margin-right: 12px;
+      }
+
+      .card__meta {
+        flex-grow: 1;
+        p,
+        h2 {
+          margin: 0;
+        }
+      }
+    }
+    .card__text {
+      margin: 16px 0;
+      padding: 0 16px;
+      box-sizing: border-box;
+    }
+
+    .card__actions {
+      display: flex;
+      justify-content: flex-end;
+      padding: 0 16px;
+      box-sizing: border-box;
+    }
   }
 </style>
