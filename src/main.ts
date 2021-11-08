c5447220cd74d7dba5556608af98bd7388fbac0c
roman <changjun19920716@gmail.com>
2021-11-08 11:31:18 +0800

    🔖  添加注释

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import router from './router'
 import store from './store'
 import 'amfe-flexible/index.js'
 
+// 全局svg-icon组件
 import SvgIconPlugin from './components/v-svg-icon/index'
+// 加载全局样式
 import '../src/styles/_module.scss'
 
 createApp(App).use(store).use(router).use(SvgIconPlugin).mount('#app')
